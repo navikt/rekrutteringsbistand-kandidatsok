1f3ac65d0f33955cdb19f2b8999ea55369f43c3e
Joar Aurdal <joar.aurdal@nav.no>
2022-09-09 16:48:22 +0200

    Legg norge bakerst i geografiquery, slik at det blir en omvendt hierarkisk oppbygning

--- src/api/query/queryMedØnsketSted.ts
+++ src/api/query/queryMedØnsketSted.ts
@@ -1,12 +1,11 @@
 const queryMedØnsketSted = (ønsketSted: Set<string>) => {
-    console.log('ønsketSted', ønsketSted);
     if (ønsketSted.size === 0) {
         return [];
     }
 
     const ønskedeSteder = Array.from(ønsketSted)
         .map((sted) => sted.split('.').slice(1))
-        .map((sted) => `${sted.join('.')}|NO|${sted[0]}${sted.length == 1 ? '.*' : ''}`)
+        .map((sted) => `${sted.join('.')}|${sted[0]}${sted.length == 1 ? '.*' : ''}|NO`)
         .map((sted) => ({
             nested: {
                 path: 'geografiJobbonsker',
