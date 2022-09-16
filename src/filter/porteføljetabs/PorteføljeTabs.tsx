287ab01a7bb5ff971d7b7e7fc1223234cfe90fad
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-09-16 12:23:24 +0200

    Prodsett kontorvelger

--- src/filter/porteføljetabs/PorteføljeTabs.tsx
+++ src/filter/porteføljetabs/PorteføljeTabs.tsx
@@ -2,9 +2,8 @@ import React, { ReactNode } from 'react';
 import { Tabs } from '@navikt/ds-react';
 import { FilterParam } from '../../hooks/useRespons';
 import useSøkekriterier from '../../hooks/useSøkekriterier';
-import css from './PorteføljeTabs.module.css';
-import { erIkkeProd } from '../../utils';
 import VelgKontorTab from './VelgKontorTab';
+import css from './PorteføljeTabs.module.css';
 
 export enum Portefølje {
     Alle = 'alle',
@@ -26,7 +25,7 @@ const PorteføljeTabs = ({ children }: { children: ReactNode }) => {
                 <Tabs.Tab value={Portefølje.Alle} label="Alle" />
                 <Tabs.Tab value={Portefølje.MineBrukere} label="Mine brukere" />
                 <Tabs.Tab value={Portefølje.MittKontor} label="Mitt kontor" />
-                {erIkkeProd && <VelgKontorTab søkekriterier={søkekriterier} />}
+                <VelgKontorTab søkekriterier={søkekriterier} />
             </Tabs.List>
             <Tabs.Panel className={css.tabpanel} value={søkekriterier.portefølje}>
                 {children}
