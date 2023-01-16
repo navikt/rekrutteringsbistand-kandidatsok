bd2b029908bba942056e4fe3f8baab7ffb03dfa6
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-01-16 11:59:48 +0100

    Oppdater ikon på sortering

--- src/kandidater/sortering/Sortering.tsx
+++ src/kandidater/sortering/Sortering.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ToggleGroup, Tooltip } from '@navikt/ds-react';
 import useSøkekriterier from '../../hooks/useSøkekriterier';
 import { FilterParam } from '../../hooks/useRespons';
-import { CoApplicant, Star } from '@navikt/ds-icons';
+import { Clock, DecisionCheck } from '@navikt/ds-icons';
 import css from './Sortering.module.css';
 import { sendEvent } from '../../amplitude';
 
@@ -37,17 +37,17 @@ const Sorteringsvalg = () => {
                 content="Sist oppdaterte kandidater kommer øverst"
             >
                 <ToggleGroup.Item value={Sortering.SisteFørst}>
-                    <CoApplicant aria-hidden />
-                    Siste først
+                    <Clock aria-hidden />
+                    Sist oppdatert
                 </ToggleGroup.Item>
             </Tooltip>
             <Tooltip
                 offset={16}
                 className={css.tooltip}
-                content="Kandidatene som passer til flest kriterier, vises øverst"
+                content="Kandidatene som oppfyller flest kriterier, vises øverst"
             >
                 <ToggleGroup.Item value={Sortering.FlestKriterier}>
-                    <Star aria-hidden /> Flest kriterier
+                    <DecisionCheck aria-hidden /> Flest kriterier
                 </ToggleGroup.Item>
             </Tooltip>
         </ToggleGroup>
