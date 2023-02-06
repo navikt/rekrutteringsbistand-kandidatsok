36a57f86e23b8595034bd24284f3b557bdb2342d
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-02-06 14:21:05 +0100

    Prøv å fikse serving av statiske filer

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ import compression from 'compression';
 
 const port = process.env.PORT || 3000;
 const basePath = '/rekrutteringsbistand-kandidatsok';
-const buildPath = path.join(__dirname, '../build');
+const buildPath = path.join(__dirname, '../dist');
 
 const app = express();
 
@@ -15,7 +15,7 @@ const startServer = () => {
         res.sendStatus(200)
     );
 
-    app.use(`${basePath}/static`, express.static(buildPath + '/static'));
+    app.use(`${basePath}/assets`, express.static(buildPath + '/assets'));
     app.use(`${basePath}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
     app.listen(port, () => {
