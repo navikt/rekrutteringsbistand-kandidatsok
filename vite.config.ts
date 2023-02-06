0c6a28ae4938cc144ed48a83cdfe1b2161074b67
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2023-02-06 13:08:36 +0100

    Tweak på Vite-bygg

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig, loadEnv, ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 
+type Proxy = Record<string, string | ProxyOptions>;
+
+const eksterneBiblioteker = [
+    'react',
+    'react-dom',
+    'react-router-dom',
+    'history',
+    '@navikt/ds-react',
+    '@navikt/ds-css',
+    '@navikt/ds-icons',
+    '@navikt/navspa',
+];
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
-    const proxyTilEs = (
-        fraPath: string,
-        tilPath: string
-    ): Record<string, string | ProxyOptions> => ({
+    const proxyTilEs = (fraPath: string, tilPath: string): Proxy => ({
         [fraPath]: {
             target: `${env.OPEN_SEARCH_URI}/veilederkandidat_current` + tilPath,
             changeOrigin: true,
@@ -24,16 +34,20 @@ export default defineConfig(({ mode }) => {
     };
 
     return {
-        plugins: [react(), svgrPlugin()],
-        base: '/rekrutteringsbistand-kandidatsok',
-        build: {
-            sourcemap: true,
-            copyPublicDir: false,
-            manifest: 'asset-manifest.json',
-        },
+        appType: 'custom',
         server: {
             port: 3005,
             proxy,
         },
+        build: {
+            sourcemap: true,
+            copyPublicDir: false,
+            manifest: 'asset-manifest.json',
+            chunkSizeWarningLimit: 1000,
+            rollupOptions: {
+                external: [],
+            },
+        },
+        plugins: [react(), svgrPlugin()],
     };
 });
